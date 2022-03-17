9d09e1ee08cb044e4da8405dbccd5d4b59c8e789
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 <nicolas@duel.com.br>
2022-03-17 09:29:29 -0300

    iniciado repositorio da materia

---
+++ aula01/exe01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exe01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
